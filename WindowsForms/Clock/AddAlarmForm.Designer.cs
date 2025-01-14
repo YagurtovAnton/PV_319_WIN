844b7e0314a34ac862695ff7bae833e803f1a8a7
YagurtovAnton <Yagur_oo18@top-academy.ru>
2025-01-14 20:50:36 +0300

    Alarm trigger DONE

--- WindowsForms/Clock/AddAlarmForm.Designer.cs
+++ WindowsForms/Clock/AddAlarmForm.Designer.cs
@@ -44,9 +44,10 @@
 			// 
 			this.cbUseDate.AutoSize = true;
 			this.cbUseDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.cbUseDate.Location = new System.Drawing.Point(13, 13);
+			this.cbUseDate.Location = new System.Drawing.Point(17, 16);
+			this.cbUseDate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 			this.cbUseDate.Name = "cbUseDate";
-			this.cbUseDate.Size = new System.Drawing.Size(117, 29);
+			this.cbUseDate.Size = new System.Drawing.Size(145, 35);
 			this.cbUseDate.TabIndex = 0;
 			this.cbUseDate.Text = "Use date";
 			this.cbUseDate.UseVisualStyleBackColor = true;
@@ -58,37 +59,41 @@
 			this.dtpDate.CustomFormat = "yyyy/MM/dd";
 			this.dtpDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
 			this.dtpDate.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-			this.dtpDate.Location = new System.Drawing.Point(13, 49);
+			this.dtpDate.Location = new System.Drawing.Point(17, 60);
+			this.dtpDate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 			this.dtpDate.Name = "dtpDate";
-			this.dtpDate.Size = new System.Drawing.Size(175, 31);
+			this.dtpDate.Size = new System.Drawing.Size(232, 37);
 			this.dtpDate.TabIndex = 1;
 			// 
 			// dtpTime
 			// 
-			this.dtpTime.CustomFormat = "hh:mm:ss tt";
+			this.dtpTime.CustomFormat = "HH:mm:ss tt";
 			this.dtpTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
 			this.dtpTime.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-			this.dtpTime.Location = new System.Drawing.Point(212, 49);
+			this.dtpTime.Location = new System.Drawing.Point(283, 60);
+			this.dtpTime.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 			this.dtpTime.Name = "dtpTime";
 			this.dtpTime.ShowUpDown = true;
-			this.dtpTime.Size = new System.Drawing.Size(175, 31);
+			this.dtpTime.Size = new System.Drawing.Size(232, 37);
 			this.dtpTime.TabIndex = 2;
 			// 
 			// lblAlarmFile
 			// 
 			this.lblAlarmFile.AutoSize = true;
-			this.lblAlarmFile.Location = new System.Drawing.Point(12, 196);
+			this.lblAlarmFile.Location = new System.Drawing.Point(16, 241);
+			this.lblAlarmFile.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
 			this.lblAlarmFile.Name = "lblAlarmFile";
-			this.lblAlarmFile.Size = new System.Drawing.Size(26, 13);
+			this.lblAlarmFile.Size = new System.Drawing.Size(32, 16);
 			this.lblAlarmFile.TabIndex = 3;
 			this.lblAlarmFile.Text = "File:";
 			// 
 			// btnOK
 			// 
 			this.btnOK.DialogResult = System.Windows.Forms.DialogResult.OK;
-			this.btnOK.Location = new System.Drawing.Point(231, 221);
+			this.btnOK.Location = new System.Drawing.Point(308, 272);
+			this.btnOK.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 			this.btnOK.Name = "btnOK";
-			this.btnOK.Size = new System.Drawing.Size(75, 23);
+			this.btnOK.Size = new System.Drawing.Size(100, 28);
 			this.btnOK.TabIndex = 4;
 			this.btnOK.Text = "OK";
 			this.btnOK.UseVisualStyleBackColor = true;
@@ -97,18 +102,20 @@
 			// btnCancel
 			// 
 			this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-			this.btnCancel.Location = new System.Drawing.Point(312, 221);
+			this.btnCancel.Location = new System.Drawing.Point(416, 272);
+			this.btnCancel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 			this.btnCancel.Name = "btnCancel";
-			this.btnCancel.Size = new System.Drawing.Size(75, 23);
+			this.btnCancel.Size = new System.Drawing.Size(100, 28);
 			this.btnCancel.TabIndex = 5;
 			this.btnCancel.Text = "Cancel";
 			this.btnCancel.UseVisualStyleBackColor = true;
 			// 
 			// btnFile
 			// 
-			this.btnFile.Location = new System.Drawing.Point(13, 221);
+			this.btnFile.Location = new System.Drawing.Point(17, 272);
+			this.btnFile.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 			this.btnFile.Name = "btnFile";
-			this.btnFile.Size = new System.Drawing.Size(75, 23);
+			this.btnFile.Size = new System.Drawing.Size(100, 28);
 			this.btnFile.TabIndex = 6;
 			this.btnFile.Text = "Choose file";
 			this.btnFile.UseVisualStyleBackColor = true;
@@ -127,35 +134,38 @@
             "Пт",
             "Сб",
             "Вс"});
-			this.clbWeekDays.Location = new System.Drawing.Point(15, 99);
+			this.clbWeekDays.Location = new System.Drawing.Point(20, 122);
+			this.clbWeekDays.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 			this.clbWeekDays.MultiColumn = true;
 			this.clbWeekDays.Name = "clbWeekDays";
-			this.clbWeekDays.Size = new System.Drawing.Size(372, 19);
+			this.clbWeekDays.Size = new System.Drawing.Size(495, 21);
 			this.clbWeekDays.TabIndex = 7;
 			// 
 			// rtbMessage
 			// 
 			this.rtbMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.rtbMessage.Location = new System.Drawing.Point(15, 151);
+			this.rtbMessage.Location = new System.Drawing.Point(20, 186);
+			this.rtbMessage.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 			this.rtbMessage.Name = "rtbMessage";
-			this.rtbMessage.Size = new System.Drawing.Size(370, 42);
+			this.rtbMessage.Size = new System.Drawing.Size(492, 51);
 			this.rtbMessage.TabIndex = 8;
 			this.rtbMessage.Text = "";
 			// 
 			// lblMessage
 			// 
 			this.lblMessage.AutoSize = true;
-			this.lblMessage.Location = new System.Drawing.Point(15, 132);
+			this.lblMessage.Location = new System.Drawing.Point(20, 162);
+			this.lblMessage.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
 			this.lblMessage.Name = "lblMessage";
-			this.lblMessage.Size = new System.Drawing.Size(112, 13);
+			this.lblMessage.Size = new System.Drawing.Size(141, 16);
 			this.lblMessage.TabIndex = 9;
 			this.lblMessage.Text = "Введите сообщение:";
 			// 
 			// AddAlarmForm
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(397, 256);
+			this.ClientSize = new System.Drawing.Size(529, 315);
 			this.Controls.Add(this.lblMessage);
 			this.Controls.Add(this.rtbMessage);
 			this.Controls.Add(this.clbWeekDays);
@@ -167,6 +177,7 @@
 			this.Controls.Add(this.dtpDate);
 			this.Controls.Add(this.cbUseDate);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+			this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 			this.Name = "AddAlarmForm";
 			this.Text = "AddAlarm";
 			this.ResumeLayout(false);
